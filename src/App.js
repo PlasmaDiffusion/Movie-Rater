2849351c99d3c9283aa1c04a85113d36a9d27363
PlasmaDiffusion <Scott50000@gmail.com>
2021-03-19 12:55:39 -0400

    Marvel movies get listed from the API + SCSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import TMDB_API from "./components/TMDB_API";
 
 function App() {
   return (
     <div className="App">
       <header> Movie Rater</header>
+      <div>
+        <TMDB_API />
+      </div>
       <footer>This site uses TMDB (themoviedb) API</footer>
     </div>
   );
