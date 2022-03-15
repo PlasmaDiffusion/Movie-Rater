8c8575b86d9841399b3e416fdb92bfa3dabd9743
Scott Cooper <Scott50000@gmail.com>
2022-03-15 19:12:29 -0400

    Listen for heroku port

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,7 @@ connection.once("open", function () {
 });
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
@@ -33,6 +34,6 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }));
 
-app.listen(4000, ()=>{
+app.listen(process.env.PORT || 4000, ()=>{
     console.log('now listening for requests on port 4000')
 });
