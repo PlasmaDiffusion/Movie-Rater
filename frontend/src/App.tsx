e9531cd91355f189a8e5b0f7c0ffe31e82eb9374
Scott Cooper <Scott50000@gmail.com>
2022-06-08 15:14:53 -0400

    Changed tab title and favicon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,34 @@
-import React from "react";
-import LoginBar from "./components/users/LoginBar";
-import MovieList from "./components/movies/movieList";
-import "./app.scss";
-
+import React from 'react';
+import { Helmet } from "react-helmet"; 
+import LoginBar from './components/users/LoginBar';
+import MovieList from './components/movies/movieList';
+import './app.scss';
 
 function App() {
-    
-        return (
-            <div className="App">
-                <header>
-                    <>
-                <LoginBar/>
-                    </>
-                </header>
-                
+  return (
+    <div className="App">
+      <Helmet>
+        <title>Movie Rater</title>
+      </Helmet>
 
-                <div>
-                    <MovieList id={1} category={"Marvel"} />
-                    <MovieList id={3} category={"DC"} />
-                    <MovieList id={10} category={"Top Grossing Films (as of 2012)"} />
-                    <MovieList id={54} category={"Japanese Animated Films"} />
-                </div>
+      <header>
+        <>
+          <LoginBar />
+        </>
+      </header>
 
-                <footer>
-                    This site uses an API from{" "}
-                    <a href="https://www.themoviedb.org/">The Movie DB</a>.
-                </footer>
-            </div>
-        );
-    
+      <div>
+        <MovieList id={1} category={'Marvel'} />
+        <MovieList id={3} category={'DC'} />
+        <MovieList id={10} category={'Top Grossing Films (as of 2012)'} />
+        <MovieList id={54} category={'Japanese Animated Films'} />
+      </div>
+
+      <footer>
+        This site uses an API from <a href="https://www.themoviedb.org/">The Movie DB</a>.
+      </footer>
+    </div>
+  );
 }
 
 export default App;
